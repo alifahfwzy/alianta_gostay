d757d7fe354ec40e7a63f452cac568c01553455f
nadadida-23 <nadadida23@gmail.com>
2025-06-17 20:00:42 +0700

    editt detaillll hottelll

--- lib/tampilan_detail_hotel.dart
+++ lib/tampilan_detail_hotel.dart
@@ -15,7 +15,7 @@ class _TampilanDetailHotelState extends State<TampilanDetailHotel> {
         title: const Text('Hotel Detail'),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
-          onPressed: () {
+          onPressed: () { 
             Navigator.pop(context);
           },
         ),
